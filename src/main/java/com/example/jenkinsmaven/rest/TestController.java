c1e32f13624f4dda9ace85dd70bc0643aed4562a
Rizwan Ahmed <rizwan.zip@gmail.com>
2023-08-07 18:22:31 +0500

    Initial commit

---
+++ src/main/java/com/example/jenkinsmaven/rest/TestController.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsmaven.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String SayHello(){
+        return "Hello Maven";
+    }
+
+}
