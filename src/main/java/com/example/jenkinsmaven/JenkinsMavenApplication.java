6356327c8425d34b125361bb38637fbf44c02a9e
Syed Rizwan Ahmed <rizwan.zip@gmail.com>
2023-09-07 06:07:32 +0500

    commit second

--- src/main/java/com/example/jenkinsmaven/JenkinsMavenApplication.java
+++ src/main/java/com/example/jenkinsmaven/JenkinsMavenApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsMavenApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(JenkinsMavenApplication.class, args);
     }
 
